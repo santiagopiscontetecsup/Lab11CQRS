2870236caeef91230aa7a45add6ab8f3bd6d5148
Santiago Joel Pisconte Chuctaya <santiago.pisconte@tecsup.edu.pe>
2025-06-16 14:59:04 -0500

    Modificacion del program

--- Lab11SantiagoPisconte.Api/Program.cs
+++ Lab11SantiagoPisconte.Api/Program.cs
@@ -12,12 +12,8 @@ builder.Services.AddHangfireServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseRouting();
